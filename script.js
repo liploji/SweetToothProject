4c5fbb5494f831ebdfc02bc55d1d731ee20c7ec6
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2023-02-16 18:30:34 -0500

    Created second navbar for small screens

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 $(document).ready(function () {
-  $("#cart1").show();
-  $("#cart2").hide();
+  $(".cart1").show();
+  $(".cart2").hide();
+  $(".menu1").show();
+  $(".menu2").hide();
   $("#cart-modal").hide();
 });
 
 $(".st-cart-button").hover(() => {
-  $("#cart1").hide();
-  $("#cart2").show();
+  $(".cart1").hide();
+  $(".cart2").show();
 });
 
 $(".st-cart-button").mouseleave(() => {
-  $("#cart1").show();
-  $("#cart2").hide();
+  $(".cart1").show();
+  $(".cart2").hide();
 });
 
 $(".st-cart-button").click(() => {
-  $("#cart-modal").show();
+  $("#cart-modal").fadeIn();
   $("#cart").animate({ right: "0px" });
 });
 
+$(".st-menu-button").hover(() => {
+  $(".menu1").hide();
+  $(".menu2").show();
+});
+
+$(".st-menu-button").mouseleave(() => {
+  $(".menu1").show();
+  $(".menu2").hide();
+});
+
+$(".st-menu-button").click(() => {
+  $("#menu-modal").fadeIn();
+  $("#menu").animate({ left: "0px" });
+});
+
 $("#cart-modal").click(() => {
-  $("#cart-modal").hide();
+  $("#cart-modal").fadeOut();
   $("#cart").animate({ right: "-300px" });
+});
+
+$("#menu-modal").click(() => {
+  $("#menu-modal").fadeOut();
+  $("#menu").animate({ right: "-300px" });
 });
