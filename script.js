37af5042bc0e5b1a847e1a39e19fa47a424c4582
MikaelaDR <m.dunnrob@gmail.com>
2023-03-24 18:48:59 -0400

    Accordion formed successfully!

--- script.js
+++ script.js
@@ -70,3 +70,18 @@ $(window).resize(() => {
 });
 
 //For reviews, create a way to calculate overall rating using given the ratings of reviews provided on the site
+
+// FAQ PAGE ACCORDION
+document.querySelectorAll('.accordion_button').forEach(button => {
+  button.addEventListener('click', () => {
+    const accordionContent = button.nextElementSibling;
+
+    button.classList.toggle('accordion_button--active');
+
+    if (button.classList.contains('accordion_button--active')) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+    } else{
+      accordionContent.style.maxHeight = 0; 
+    }
+  });
+});
